cc12ac3af5105acf32a2e1d393e5c6dcdbce59b7
Geert Fannes <geert.fannes@auro-technologies.com>
2015-11-10 16:05:01 +0100

    wip

--- rakefile.rb
+++ rakefile.rb
@@ -10,11 +10,12 @@ task :help do
 end
 
 task :declare do
+    build_ok_fn = 'gubg.build.ok'
     Dir.chdir(shared_dir('extern')) do
         git_clone('https://github.com/SFML', 'SFML') do
-            if !File.exist?('build')
-                Dir.mkdir('trybuild')
-                Dir.chdir('trybuild') do
+            Dir.mkdir('build') unless File.exist?('build')
+            Dir.chdir('build') do
+                if !File.exist?(build_ok_fn)
                     #For ubuntu, these packages should be installed:
                     #sudo apt-get install freeglut3
                     #sudo apt-get install freeglut3-dev
@@ -25,12 +26,12 @@ task :declare do
                     #sudo apt-get install libopenal-dev
                     #sudo apt-get install libvorbis-dev
                     #sudo apt-get install libflac-dev
-                    sh 'cmake ..'
+                    sh 'cmake .. -DBUILD_SHARED_LIBS=FALSE'
                     sh 'make -j 4'
+                    sh "touch #{build_ok_fn}"
                 end
-                mv('trybuild', 'build')
             end
-            publish('build/lib', '*.so', dst: 'lib')
+            publish('build/lib', '*.a', dst: 'lib'){|fn|fn.gsub(/-s\.a$/, '.a')}
             publish('include', '**/*', dst: 'include')
         end
     end
@@ -38,6 +39,7 @@ end
 
 task :define => :declare do
     exe = Build::Executable.new('test.exe')
+    exe.add_include_path(shared_dir('include'))
     exe.add_sources('src/test/test.cpp')
     exe.add_library_path(shared_dir('lib'))
     exe.add_library('sfml-system', 'sfml-window', 'sfml-graphics')
