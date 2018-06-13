f5cadc5d95ca1f73344eec0b06fffe45f9e34fcc
Geert Fannes <geert.fannes@auro-technologies.com>
2018-06-13 10:30:53 +0200

    wip

--- src/gubg/imgui/SelectFile.hpp
+++ src/gubg/imgui/SelectFile.hpp
@@ -7,36 +7,56 @@
 
 namespace gubg { namespace imgui { 
 
-    inline bool select_file(const char *str_id, std::filesystem::path &filename)
+    inline bool select_file(const char *str_id, std::filesystem::path &path)
     {
-        State state(str_id);
-        if (state.first())
-        {
-            state.directory(std::filesystem::current_path());
-        }
+        bool file_was_selected = false;
 
-        auto dir = state.directory();
+        State state(str_id);
 
         if (ImGui::Button(str_id))
             state.active(!state.active());
 
-        bool file_was_selected = false;
-
         if (state.active())
         {
+            path = path.make_preferred();
+            if (!std::filesystem::exists(path))
+                path = std::filesystem::current_path();
+
+            std::filesystem::path dir = path;
+            std::filesystem::path fn;
+            if (!std::filesystem::is_directory(dir))
+            {
+                fn = dir.filename();
+                dir.remove_filename();
+            }
+
             ImGui::Begin("Select file");
 
             if (ImGui::Button("Cancel"))
                 state.active(false);
+            if (!fn.empty())
+            {
+                ImGui::SameLine();
+                if (ImGui::Button("Select"))
+                {
+                    file_was_selected = true;
+                    state.active(false);
+                }
+            }
 
             if (ImGui::Button(".."))
             {
                 auto new_dir = dir.parent_path();
                 if (!new_dir.empty())
+                {
                     dir = new_dir;
+                    fn.clear();
+                }
             }
             ImGui::SameLine();
             ImGui::Text("directory: %s", dir.string().c_str());
+            ImGui::SameLine();
+            ImGui::Text("filename: %s", fn.string().c_str());
 
             auto size = ImGui::GetContentRegionAvail();
             size.x *= 0.5;
@@ -48,10 +68,13 @@ namespace gubg { namespace imgui {
                 {
                     for (const auto &dirent: std::filesystem::directory_iterator(dir))
                     {
-                        const auto new_dir = dirent.path();
-                        if (is_directory(new_dir))
-                            if (ImGui::Selectable(new_dir.string().c_str()))
-                                dir = new_dir;
+                        auto p = dirent.path();
+                        if (is_directory(p))
+                        {
+                            p = p.filename();
+                            if (ImGui::Selectable(p.string().c_str()))
+                                dir /= p;
+                        }
                     }
                 }
                 catch (const std::exception& exc)
@@ -68,14 +91,13 @@ namespace gubg { namespace imgui {
                 {
                     for (const auto &dirent: std::filesystem::directory_iterator(dir))
                     {
-                        const auto fn = dirent.path();
-                        if (is_regular_file(fn))
-                            if (ImGui::Selectable(fn.string().c_str()))
-                            {
-                                filename = fn;
-                                file_was_selected = true;
-                                state.active(false);
-                            }
+                        auto p = dirent.path();
+                        if (is_regular_file(p))
+                        {
+                            p = p.filename();
+                            if (ImGui::Selectable(p.string().c_str()))
+                                fn = p;
+                        }
                     }
                 }
                 catch (const std::exception& exc)
@@ -87,9 +109,9 @@ namespace gubg { namespace imgui {
             ImGui::PopItemWidth();
 
             ImGui::End();
-        }
 
-        state.directory(dir);
+            path = dir / fn;
+        }
 
         return file_was_selected;
     }
